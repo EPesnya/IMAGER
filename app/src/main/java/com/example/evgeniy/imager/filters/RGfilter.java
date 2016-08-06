365ec1b3a86bbe81d75be3a032c71c46fd87ea6b
PesnyaEvgeniy <pesnyaevgeniy@gmail.com>
2016-08-06 16:02:22 +0300

    сохранил

--- app/src/main/java/com/example/evgeniy/imager/filters/RGfilter.java
+++ app/src/main/java/com/example/evgeniy/imager/filters/RGfilter.java
@@ -1,7 +1,45 @@
 package com.example.evgeniy.imager.filters;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+
+import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool;
+import com.bumptech.glide.load.resource.bitmap.BitmapTransformation;
+
 /**
  * Created by Evgeniy on 8/3/2016.
  */
-public class RGfilter {
+public class RGfilter extends BitmapTransformation {
+
+    public RGfilter(Context context) {
+        super(context);
+    }
+
+
+
+    @Override
+    protected Bitmap transform(BitmapPool pool, Bitmap toTransform,
+                               int outWidth, int outHeight) {
+        Bitmap myTransformedBitmap = toTransform.copy( Bitmap.Config.ARGB_8888, true );
+        int width = toTransform.getWidth();
+        int height = toTransform.getHeight();
+        int[] srcPixels = new int[width * height];
+        int[] destPixels = new int[width * height];
+        myTransformedBitmap.getPixels(srcPixels, 0, width, 0, 0, width, height);
+
+        for (int i = 0; i < srcPixels.length; i++) {
+            destPixels[i] = (srcPixels[i] & 0xff0000ff) | ((srcPixels[i] & 0x0000ff00) << 8)
+                    | ((srcPixels[i] & 0x00ff0000) >> 8);
+        }
+
+        myTransformedBitmap.setPixels(destPixels, 0, width, 0, 0, width, height);
+
+        return myTransformedBitmap;
+    }
+
+    @Override
+    public String getId() {
+        // Return some id that uniquely identifies your transformation.
+        return "com.example.myapp.MyTransformation6";
+    }
 }
--- app/src/main/java/com/example/evgeniy/imager/filters/TestFilter.java
+++ app/src/main/java/com/example/evgeniy/imager/filters/TestFilter.java
@@ -1,7 +1,45 @@
 package com.example.evgeniy.imager.filters;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+
+import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool;
+import com.bumptech.glide.load.resource.bitmap.BitmapTransformation;
+
 /**
  * Created by Evgeniy on 8/4/2016.
  */
-public class TestFilter {
+public class TestFilter extends BitmapTransformation {
+
+    public TestFilter(Context context) {
+        super(context);
+    }
+
+
+
+    @Override
+    protected Bitmap transform(BitmapPool pool, Bitmap toTransform,
+                               int outWidth, int outHeight) {
+        Bitmap myTransformedBitmap = toTransform.copy( Bitmap.Config.ARGB_8888, true );
+        int width = toTransform.getWidth();
+        int height = toTransform.getHeight();
+        int[] srcPixels = new int[width * height];
+        int[] destPixels = new int[width * height];
+        myTransformedBitmap.getPixels(srcPixels, 0, width, 0, 0, width, height);
+
+        for (int i = 0; i < srcPixels.length; i++) {
+            destPixels[i] = (srcPixels[i] & 0xff00ff00) | ((srcPixels[i] & 0x000000ff) << 16)
+                    | ((srcPixels[i] & 0x00ff0000) >> 16);
+        }
+
+        myTransformedBitmap.setPixels(destPixels, 0, width, 0, 0, width, height);
+
+        return myTransformedBitmap;
+    }
+
+    @Override
+    public String getId() {
+        // Return some id that uniquely identifies your transformation.
+        return "com.example.myapp.MyTransformation7";
+    }
 }
